c831d2fd19311763fae07d5d0d5284854521aee6
Ferdhika Yudira <rpl4rt08@gmail.com>
2021-08-31 21:45:51 +0700

    get summary mata pelajaran

--- app/Http/Controllers/API/MataPelajaranController.php
+++ app/Http/Controllers/API/MataPelajaranController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\MataPelajaran;
+use App\Models\Video;
+use App\Models\HistoryVideo;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\MataPelajaran as MataPelajaranResource;
+use App\Http\Resources\Summary as SummaryResource;
    
 class MataPelajaranController extends BaseController
 {
@@ -73,5 +76,62 @@ class MataPelajaranController extends BaseController
         }
    
         return $this->sendResponse(new MataPelajaranResource($data), 'MataPelajaran retrieved successfully.');
+    }
+
+    /**
+     * Display summary the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function summary($id)
+    {
+        $mapel = MataPelajaran::with('kelas.tingkat')->find($id);
+  
+        if (is_null($mapel)) {
+            return $this->sendError('MataPelajaran not found.');
+        }
+
+        // init var
+        $totalModul = 0;
+        $doneModul = 0;
+        $totalSimulasi = 0;
+        $doneSimulasi = 0;
+
+        // counting video by mapel
+        $videos = Video::where('mata_pelajaran_id', $id)->get();
+        $totalVideo = count($videos);
+        // video watched by siswa
+        $videoHistory = HistoryVideo::where('siswa_id', Auth::user()->id);
+        // video history by mapel
+        $videoHistory = $videoHistory->whereHas('video', function($query) use ($id) {
+            $query->where('mata_pelajaran_id', $id);
+        });
+        $videoHistory = $videoHistory->get();
+        $doneVideo = count($videoHistory);
+
+        $data = [
+            'progress_modul' => [
+                'percentage' => $this->calculatePercentage($totalModul, $doneModul),
+                'total' => $totalModul,
+                'done' => $doneModul,
+            ],
+            'progress_video' => [
+                'percentage' => $this->calculatePercentage($totalVideo, $doneVideo),
+                'total' => $totalVideo,
+                'done' => $doneVideo,
+            ],
+            'progress_simulasi' => [
+                'percentage' => $this->calculatePercentage($totalSimulasi, $doneSimulasi),
+                'total' => $totalSimulasi,
+                'done' => $doneSimulasi,
+            ],
+        ];
+   
+        return $this->sendResponse(new SummaryResource($data), 'Summary retrieved successfully.');
+    }
+
+    private function calculatePercentage($total, $done){
+        return ($done === 0 || $total === 0) ? 0 : ($done/$total) * 100;
     }
 }
