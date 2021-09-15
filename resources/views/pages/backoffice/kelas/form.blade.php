00209ef5f563489ef4e0cead354c9b32cad5c671
Ferdhika Yudira <rpl4rt08@gmail.com>
2021-09-15 18:04:54 +0700

    add select2 to form kelas & mapel

--- resources/views/pages/backoffice/kelas/form.blade.php
+++ resources/views/pages/backoffice/kelas/form.blade.php
@@ -11,3 +11,20 @@
         <a href="{{route("backoffice::kelas.index")}}" class="btn btn-sm btn-secondary mr-2">@lang("Cancel")</a>
     </div>
 </div>
+
+
+@push('plugin_script')
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#tingkat_id').select2();
+    });
+</script>
+@endpush
+
+@push('script')
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+@endpush
+
+@push('plugin_css')
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+@endpush
