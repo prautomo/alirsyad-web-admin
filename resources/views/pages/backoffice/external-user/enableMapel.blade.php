d5d1216f92d0345a437d591c6868138738f7c7f1
Ferdhika Yudira <rpl4rt08@gmail.com>
2021-10-29 20:56:39 +0700

    add choose semua mapel per group tingkat

--- resources/views/pages/backoffice/external-user/enableMapel.blade.php
+++ resources/views/pages/backoffice/external-user/enableMapel.blade.php
@@ -7,17 +7,8 @@
                 <div class="form-group">
                     <label class="form-control-label" for="input-mapel">Mata Pelajaran</label>
 
-                    <select id="mapel" multiple="multiple" name="mapel[]" class="js-example-basic-multiple form-control {{($errors->has('mapel') ? ' is-invalid' : '')}}">
-                        @foreach(@$mapelList as $idx => $mapel)
-
-                        @if(in_array($idx, $mapelIDS))
-                        <option value="{{ $idx }}" selected="true">{{ $mapel }}</option>
-                        @else
-                        <option value="{{ $idx }}">{{ $mapel }}</option>
-                        @endif 
-
+                    <select id="mapel" multiple="multiple" name="mapel[]" class="form-control {{($errors->has('mapel') ? ' is-invalid' : '')}}">
                         
-                        @endforeach
                     </select>
 
 
@@ -38,6 +29,14 @@
 
     @push('plugin_css')
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+    <style>
+        li.select2-results__option strong.select2-results__group:hover {
+        background-color: #ddd;
+        cursor: pointer;
+        }
+
+    </style>
     @endpush
 
 
@@ -48,7 +47,76 @@
     @push('plugin_script')
     <script type="text/javascript">
         $(document).ready(function() {
-            $('.js-example-basic-multiple').select2();
+            // $('.js-example-basic-multiple').select2();
+
+            var countries = <?php echo json_encode($mapelList); ?>
+
+            var s2 = $('#mapel').select2({
+                placeholder: "Please select mapel",
+                allowClear: true,
+                width: '100%',
+                data: countries,
+            });
+
+            var vals = <?php echo json_encode($mapelIDS); ?>;
+
+            vals.forEach(function(e){
+                if(!s2.find('option:contains(' + e + ')').length) 
+                s2.append();
+            });
+
+            s2.val(vals).trigger("change"); 
+
+        $('#mapel').on('select2:open', function(e) {
+
+        $('#select2-mapel-results').on('click', function(event) {
+
+            event.stopPropagation();
+            var data = $(event.target).html();
+            var selectedOptionGroup = data.toString().trim();
+
+            var groupchildren = [];
+
+            for (var i = 0; i < countries.length; i++) {
+                if (selectedOptionGroup.toString() === countries[i].text.toString()) {
+                    for (var j = 0; j < countries[i].children.length; j++) {
+                        groupchildren.push(countries[i].children[j].id);
+                    }
+                }
+            }
+
+            var options = [];
+            options = $('#mapel').val();
+
+            if (options === null || options === '') {
+
+            options = [];
+
+            }
+
+            for (var i = 0; i < groupchildren.length; i++) {
+
+                var count = 0;
+
+                for (var j = 0; j < options.length; j++) {
+                    if (options[j].toString() === groupchildren[i].toString()) {
+                        count++;
+                        break;
+                    }
+                }
+
+                if (count === 0) {
+                    options.push(groupchildren[i].toString());
+                }
+            }
+
+            $('#mapel').val(options);
+            $('#mapel').trigger('change'); // Notify any JS components that the value changed
+            $('#mapel').select2('close');    
+
+        });
+        });
+
         });
     </script>
     @endpush
