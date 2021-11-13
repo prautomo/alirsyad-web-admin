cf295088fe8becd69c1d82ae6ca93570db1831b5
Ferdhika Yudira <rpl4rt08@gmail.com>
2021-11-13 19:22:07 +0700

    list percobaan nilai siswa

---
+++ app/Http/Controllers/Guru/SimulasiController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Guru;
+
+use App\Http\Controllers\Controller;
+use App\Models\Simulasi;
+use App\Models\Score;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use App\Http\Resources\Score as ScoreResource;
+use Illuminate\Support\Facades\Hash;
+
+class SimulasiController extends Controller
+{
+    
+}
