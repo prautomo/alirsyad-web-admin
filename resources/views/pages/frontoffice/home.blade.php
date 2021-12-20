856d32ccac34c0e3a8b7c91c0727659123b74516
Ferdhika Yudira <rpl4rt08@gmail.com>
2021-12-20 09:38:35 +0700

    layout dummy home siswa

--- resources/views/pages/frontoffice/home.blade.php
+++ resources/views/pages/frontoffice/home.blade.php
@@ -4,13 +4,44 @@
 
 @section('content')
 <!-- Slider Start -->
-<section class="slider">
-        <div class="container">
+    <section class="slider">
+        <div class="">
             <div class="row">
-                <div class="col-lg-9 col-md-10">
+                <div class="col-lg-12 col-md-12">
                     <div class="block">
-                        <span class="d-block mb-3 text-white text-capitalize">Lorem ipsum dolor sit amet</span>
-                        <h1 class="animated fadeInUp mb-5">Our work is <br>presentation of our <br>capabilities.</h1>
+                        <!-- <span class="d-block mb-3 text-white text-capitalize">Lorem ipsum dolor sit amet</span>
+                        <h1 class="animated fadeInUp mb-5">Our work is <br>presentation of our <br>capabilities.</h1> -->
+
+                        <div id="top-banner" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#top-banner" data-slide-to="0" class="active"></li>
+                            <li data-target="#top-banner" data-slide-to="1"></li>
+                            <li data-target="#top-banner" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="https://via.placeholder.com/1000x250?text=Slide+1+1000x250" class="d-block w-100" alt="Slide">
+                                <!-- <div class="carousel-caption d-none d-md-block">
+                                    <h5 class="text-white">Lorem ipsum dolor sit amet</h5>
+                                    <p class="text-white">Lorem ipsum dolor sit amet</p>
+                                </div> -->
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://via.placeholder.com/1000x250?text=Slide+2+1000x250" class="d-block w-100" alt="Slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://via.placeholder.com/1000x250?text=Slide+3+1000x250" class="d-block w-100" alt="Slide">
+                            </div>
+                        </div>
+                        <!-- <a class="carousel-control-prev" href="#top-banner" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#top-banner" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a> -->
+                        </div>
                     </div>
                 </div>
             </div>
@@ -20,13 +51,13 @@
 
     <section class="mt-5">
         <div class="container">
-            <div class="row">
+            <div class="row mb-2">
                 <!-- Buat siswa -->
                 @if(!@\Auth::user()->is_pengunjung)
                 <div class="col-md-12 mb-1">
                     <div class="card-title mb-0 form-inline">
                         <i class="ni ni-books icon-title"></i>
-                        <div>Mata Pelajaran yang Sedang Dipelajari</div>
+                        <div class="font-weight-bolder" style="color: #0E594D;">Mata Pelajaran yang Sedang Dipelajari</div>
                     </div>
                     <hr/>
                 </div>
@@ -167,6 +198,91 @@
                 </div>
                 @endif
             </div>
+
+            <!-- khusus siswa -->
+            @if(!@\Auth::user()->is_pengunjung)
+            <!-- Update terbaru -->
+            <div class="row my-5">
+                <div class="col-md-2 my-auto">
+                    <h4 class="font-weight-bolder" style="color: #0E594D;">Update Terbaru</h4>
+                </div>
+
+                <div class="col-md-10">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators indicator-custom">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <div class="card">
+                                    <div class="card-body">
+                                        Lorem ipsum dolor sit amet
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="card">
+                                    <div class="card-body">
+                                        Lorem ipsum dolor sit amet 2
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End update terbaru -->
+
+            <!-- Semua Mata Pelajaran -->
+            <div class="row">
+                <!-- <div class="col-md-12 mb-1">
+                    <div class="card-title mb-0 form-inline">
+                        <i class="ni ni-books icon-title"></i>
+                        <div class="font-weight-bolder" style="color: #0E594D;">Semua Mata Pelajaran</div>
+                    </div>
+                    <hr/>
+                </div>
+                
+                <div class="col-md-2">
+                    <div class="card">
+                        <div class="card-body mx-auto my-auto">
+                            <span class="font-weight-500">
+                                Kelas 1
+                            </span>
+                        </div>
+                    </div>
+                </div> -->
+                
+            </div>
+            <!-- End Semua Mata Pelajaran -->
+            @endif
         </div>
 </section>
 @endsection
+
+@push('style')
+<style>
+    .indicator-custom li {
+        background-color: #000;
+    }
+</style>
+@endpush
+
+@push('script')
+<script type="text/javascript">
+    $(function() {
+        $(".carousel-items").each(function() {
+            var i = $(this).next();
+            console.log("dika i", i)
+            i.length || (i = $(this).siblings(":first")),
+                i.children(":first-child").clone().appendTo($(this));
+            
+            for (var n = 0; n < 4; n++)(i = i.next()).length ||
+                (i = $(this).siblings(":first")),
+                i.children(":first-child").clone().appendTo($(this))
+        })
+    });
+</script>
+@endpush
