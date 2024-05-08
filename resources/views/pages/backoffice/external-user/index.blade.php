736a1b91a3ec26394299f32f05edd592b693a922
Ferdhika Yudira <rpl4rt08@gmail.com>
2024-05-08 19:54:51 +0700

    chore: fix style filter

--- resources/views/pages/backoffice/external-user/index.blade.php
+++ resources/views/pages/backoffice/external-user/index.blade.php
@@ -6,20 +6,28 @@
   @parent
     <div class="row align-items-center py-4">
         <div class="col-lg-6 col-7">
-          <form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main">
-            <div class="form-group mb-0">
-              <div class="input-group input-group-alternative input-group-merge">
-                <div class="input-group-prepend">
-                  <span class="input-group-text"><i class="fas fa-search"></i></span>
+          <div class="d-flex align-items-center">
+            <form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main">
+              <div class="form-group mb-0">
+                <div class="input-group input-group-alternative input-group-merge">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-search"></i></span>
+                  </div>
+                  <input class="form-control" id="search-dt-table" placeholder="Cari NIS, Nama, Jenjang" type="text">
                 </div>
-                <input class="form-control" id="search-dt-table" placeholder="Cari NIS, Nama, Jenjang" type="text">
               </div>
-            </div>
-            <button type="button" class="close" data-action="search-close" data-target="#navbar-search-main"
-              aria-label="Close">
-              <span aria-hidden="true">×</span>
-            </button>
-          </form>
+              <button type="button" class="close" data-action="search-close" data-target="#navbar-search-main"
+                aria-label="Close">
+                <span aria-hidden="true">×</span>
+              </button>
+            </form>
+
+            <a href="#" data-toggle="modal" data-target="#filterModal" class="btn btn-md btn-icon btn-outline-primary ml-3" >
+                {{__("Filter")}}
+                &nbsp; &nbsp;
+                <i class="fa fa-sort-down align-text-top"></i>
+            </a>
+          </div>
         </div>
         @can('external-user-create')
         <div class="col-lg-6 col-5 text-right">
