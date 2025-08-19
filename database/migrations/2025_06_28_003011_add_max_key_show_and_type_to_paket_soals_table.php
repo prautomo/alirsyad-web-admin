8717e04a427b88cf1367e833f7f433a7f61deafe
Ferdhika Yudira <rpl4rt08@gmail.com>
2025-08-19 15:38:29 +0700

    fix: class name migration

--- database/migrations/2025_06_28_003011_add_max_key_show_and_type_to_paket_soals_table.php
+++ database/migrations/2025_06_28_003011_add_max_key_show_and_type_to_paket_soals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnMaxShowAnswerKeyAndTypeToPaketSoalsTable extends Migration
+class AddMaxKeyShowAndTypeToPaketSoalsTable extends Migration
 {
     /**
      * Run the migrations.
