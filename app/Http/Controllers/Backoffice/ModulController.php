807e4af162c00744f71342f3bbda91f650f267ce
unknown <windahidayat27@gmail.com>
2025-09-01 20:26:57 +0700

    fix(content): order by mata_pelajaran_id, semester, and urutan

--- app/Http/Controllers/Backoffice/ModulController.php
+++ app/Http/Controllers/Backoffice/ModulController.php
@@ -157,7 +157,7 @@ class ModulController extends Controller
                 ]);
             })
             ->order(function ($query) {
-                $query->orderBy('created_at', 'desc');
+                $query->orderBy('mata_pelajaran_id', 'asc')->orderBy('semester', 'asc')->orderBy('urutan', 'asc')->orderBy('created_at', 'desc');
             })
             ->toJson();
     }
